c499b351ebae307e075b05fb4e7dacd972683921
Михаил Борисов <mdborisov@sberbank.ru>
2021-12-20 20:23:14 +0300

    CROW-9: Создать пустой проект в Xcode

---
+++ Crowdy/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Crowdy
+//
+//  Created by Михаил Борисов on 20.12.2021.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+}
+
